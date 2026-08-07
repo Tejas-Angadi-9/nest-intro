464723b4545a3a76bd45141ed354a061d419e59b
Tejas Angadi <tejasangadi456@gmail.com>
2026-08-07 08:38:52 +0530

    feat: add user management functionality with controller and request definitions

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,5 +1,15 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Post } from '@nestjs/common';
 
 @Controller('users')
-class UsersController {}
+class UsersController {
+  @Get()
+  public getUsers() {
+    return 'You sent a get request to users endpoint';
+  }
+
+  @Post()
+  public createUsers() {
+    return 'You sent a post request to create new user';
+  }
+}
 export default UsersController;
