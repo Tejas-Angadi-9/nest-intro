7e9d033ed276718c52cbe1da0f9cf8c37f328672
Tejas Angadi <tejasangadi456@gmail.com>
2026-08-07 09:19:47 +0530

    feat: enhance user controller with improved parameter handling and logging

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,16 +1,31 @@
-import { Controller, Get, Post, Param, Query, Body } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Post,
+  Param,
+  Query,
+  Body,
+  Headers,
+  Ip,
+} from '@nestjs/common';
 
 @Controller('users')
 class UsersController {
   @Get('/:id{/:optional}')
-  public getUser(@Param() params: any, @Query() query: any) {
-    console.log(query);
+  public getUser(@Param('id') id: any, @Query('limit') limit: any) {
+    console.log({ limit });
 
-    return `You sent a GET request to get user data ${params.id} and ${params.optional}`;
+    return `You sent a GET request to get user data ${id}`;
   }
 
   @Post()
-  public createUsers(@Body() request: any) {
+  public createUsers(
+    @Body() request: any,
+    @Headers() headers: any,
+    @Ip() ip: any,
+  ) {
+    console.log('Headers: ', headers);
+    console.log('IP: ', ip);
     console.log(request);
     return 'You sent a POST request to create new user';
   }
